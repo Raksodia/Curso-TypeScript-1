77ced3024fe79706916e55fbe219053148335ef5
Williamns Robles <robles.williamns@gmail.com>
2022-09-30 00:11:00 -0500

    Interface
    Interface

---
+++ src/array-interface.ts
@@ -0,0 +1,8 @@
+/********************************/
+
+interface Persona2{
+    nombre: string
+}
+const arregloPersona: Array<Persona2> = [{nombre:'Pablo'},{nombre:'Pedro'}];
+
+/********************************/

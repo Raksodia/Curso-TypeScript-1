28e22a1f0b332944eb652dbc936098fe1d05c947
Williamns Robles <robles.williamns@gmail.com>
2022-09-30 00:10:29 -0500

    Class - Array
    Class - Array

---
+++ src/class-array.ts
@@ -0,0 +1,8 @@
+/********************************/
+
+class Persona1{};
+let persona = new Persona1();
+const arregloString1 : string[] = ['a','b','c'];
+const arregloStringq : Array<string> = ['a','b','c'];
+
+/********************************/

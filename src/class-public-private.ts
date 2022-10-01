136166fda99c023f5a55f80a10f5e45ff0f067d2
Williamns Robles <robles.williamns@gmail.com>
2022-10-01 18:11:25 -0500

    Class - Properties Public and Private
    Class - Properties Public and Private

---
+++ src/class-public-private.ts
@@ -0,0 +1,31 @@
+// Propiedades públicas y privadas en clases.
+
+type Color1 = 'negro' | 'rojo' | 'azul' | 'amarillo';
+
+class Vehiculo1 {
+
+    _marca: string;
+    _color: Color1;
+    _numeroRuedas: number;
+
+    constructor(marca: string, color: Color1, numeroRuedas: number) {
+        this._marca = marca;
+        this._color = color;
+        this._numeroRuedas = numeroRuedas;
+    }
+
+    descripcionVehiculo(): string {
+        return `El vehículo es de marca ${this._marca} y es de color ${this._color}`;
+    }
+
+    moverse(): string {
+        return `El vehículo se mueve a una velocidad de ${this.obtenerVelocidad()}`;
+    }
+
+    private obtenerVelocidad(): string {
+        return '100 km/h';
+    }
+
+}
+
+const miVehiculo1 = new Vehiculo('Nissan', 'negro', 4);
